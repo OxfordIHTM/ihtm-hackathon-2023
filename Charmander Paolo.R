91a6915cb8b784a4aad33663eb0f6caec63aa4b8
johannes paolo cerrado <jcerrado@gmail.com>
2023-02-15 09:05:37 +0000

    established base file read table

---
+++ Charmander Paolo.R
@@ -0,0 +1,4 @@
+# Team Charmander - IHTM HACKATHON
+## reading the data set
+nut <- read.csv(file = "https://github.com/OxfordIHTM/ihtm-hackathon-2023/blob/1adce1d1528de080be09f5ab1e646aae50ae5705/data/nutrition_survey_data.csv")
+nut
