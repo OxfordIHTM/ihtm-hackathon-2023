5ea31e4a5537f9d45e0ec102be750fe83b15977d
ashishlwh-giri <122526275+ashishlwh-giri@users.noreply.github.com>
2023-02-15 09:44:24 +0000

    Hackathon_day1_question1

--- coverage_assessment.R
+++ coverage_assessment.R
@@ -3,7 +3,8 @@
 # MSc IHTM Hackathon 2023
 #
 ################################################################################
-
+#Install Dplyr
+install.packages("dplyr")
 # Load libraries ----
 
 ## Load R packages ----
@@ -18,14 +19,15 @@ nut <- get_data()
 
 
 # Problem 1: Describe the data ----
-
-
-
-
-
-
-
-
+##Number of children in Baseline
+A <- sum(nut$survey_round == "Baseline")
+##Number of children in Endline
+B <- sum(nut$survey_round == "Endline")
+nrow("survey_round")
+sum(A+B)
+##Checking the total number of data points in survey round
+length(nut$survey_round)
+#So there are no entries other than baseline and endline in survey_round column
 
 
 # Problem 2: Screening coverage ----
