a158fdd03050dbb677beeef26ea206c1a3ef3e0e
Sophia Adhikari <sophiaadhikari234@gmail.com>
2023-02-15 10:21:41 +0000

    Preliminary review for first 2 questions of team Pikachu

--- coverage_assessment.R
+++ coverage_assessment.R
@@ -41,10 +41,39 @@ nut <- get_data()
 
 # Problem 3: Prevalence of acute undernutrition/malnutrition ----
 
+library(dplyr)
+
+## Load functions in R directory ----
+for (f in list.files("R", full.names = TRUE)) source (f)
 
 
+# Read data ----
+nut <- get_data()
 
 
+with(nut, sum(muac >= 11.5 & muac <12.5 & oedema == 2, na.rm= TRUE))
+
+##number of moderate acute malnourished (MAM) cases children 6-59 months old 
+
+
+with(nut, sum(muac >= 11.5 & muac <12.5 & oedema == 2 & sex == 1 , na.rm= TRUE ))
+
+##number of moderate acute malnourished (MAM) male 6-59 months old 
+
+
+with(nut, sum(muac >= 11.5 & muac <12.5 & oedema == 2 & sex == 2 , na.rm= TRUE ))
+
+##number of moderate acute malnourished (MAM) female 6-59 months old 
+
+
+with(nut, sum(muac >= 11.5 & muac <12.5 & oedema == 2 & county == "Urban Montserrado" , na.rm= TRUE))
+
+##number of moderate acute malnourished (MAM) cases children 6-59 months old  in Urban Montserrado
+
+
+with(nut, sum(muac >= 11.5 & muac <12.5 & oedema == 2 & county != "Urban Montserrado" , na.rm= TRUE))
+
+##number of moderate acute malnourished (MAM) cases children 6-59 months old other than Urban Montserrado
 
 
 
