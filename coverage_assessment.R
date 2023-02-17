63928299cec4d5449e60ea019ff51a348e4a8b35
Sophia Adhikari <sophiaadhikari234@gmail.com>
2023-02-17 23:09:10 +0000

    Alternate table method by sex and by location

--- coverage_assessment.R
+++ coverage_assessment.R
@@ -43,23 +43,23 @@ nut <- get_data()
 
 ####MAM
 
-with(nut, sum(muac >= 11.5 & muac <12.5  & age >=6 & age <=59 & oedema == 2, na.rm= TRUE))
+with(nut, sum(muac >= 11.5 & muac <12.5  & age >=6 & age <60 & oedema == 2, na.rm= TRUE))
 
 ##number of moderate acute malnourished (MAM) cases children 6-59 months old 
 
-Male_MAM <- with(nut, sum(muac >= 11.5 & muac <12.5 & age >=6 & age <=59 & oedema == 2 & sex == 1 , na.rm= TRUE ))
+Male_MAM <- with(nut, sum(muac >= 11.5 & muac <12.5 & age >=6 & age <60 & oedema == 2 & sex == 1 , na.rm= TRUE ))
 
 ##assigning moderate acute malnourished (MAM) male 6-59 months old to Male_MAM
 
-Female_MAM <- with(nut, sum(muac >= 11.5 & muac <12.5 & age >=6 & age <=59 & oedema == 2 & sex == 2 , na.rm= TRUE ))
+Female_MAM <- with(nut, sum(muac >= 11.5 & muac <12.5 & age >=6 & age <60 & oedema == 2 & sex == 2 , na.rm= TRUE ))
 
 ##assigning  moderate acute malnourished (MAM) female 6-59 months old to Female_MAM
 
-UM_MAM <- with(nut, sum(muac >= 11.5 & muac <12.5 & age >=6 & age <=59 & oedema == 2 & county == "Urban Montserrado" , na.rm= TRUE))
+UM_MAM <- with(nut, sum(muac >= 11.5 & muac <12.5 & age >=6 & age <60 & oedema == 2 & county == "Urban Montserrado" , na.rm= TRUE))
 
 ##assigning moderate acute malnourished (MAM) cases children 6-59 months old  in Urban Montserrado to UM_MAM
 
-GB_MAM <- with(nut, sum(muac >= 11.5 & muac <12.5 &age >=6 & age <=59 & oedema == 2 & county != "Urban Montserrado" , na.rm= TRUE))
+GB_MAM <- with(nut, sum(muac >= 11.5 & muac <12.5 &age >=6 & age <60 & oedema == 2 & county == "Grand Bassa" , na.rm= TRUE))
 
 ##assigning  moderate acute malnourished (MAM) cases children 6-59 months old other than Urban Montserrado to GB_MAM
 
@@ -71,12 +71,40 @@ if(interactive()) View(MAM)
 
 ##Viewing MAM data
 
-den1 <- with(nut, sum(age >=5 & age < 60, na.rm = TRUE ))
 
-##assigning total number of children aged 5-60 months old to den1
+
+####Alternate table method for number of moderate,severe and normal malnutrition by sex and by location:
+
+num_data <- with(nut, data.frame(sex,county,muac,oedema))
+
+##assigning the data frame to num_data
+
+SEX <- ifelse(my_data$sex == "1", "Male", "Female")
+OEDEMA <- ifelse(my_data$oedema == "1", "Yes", "No")
+MUAC <- with( my_data, ifelse (muac <11.5, "Severe",
+                               ifelse(oedema == 1, "Severe",
+                                      ifelse(muac >=11.5 & muac<12.5, "Moderate",
+                                             ifelse (muac >=12.5, "Normal", "No")))))
+
+list <- data.frame(SEX,MUAC, OEDEMA, num_data$county)
+
+##creating data frame by sex,muac,oedema and location
+
+
+tab <-table(SEX,my_data$county, MUAC )
+
+##creating table showing different numbers of children according to moderate,severe degree of Malnutrition and normal children
+
+tab
+
+##viewing table 
 
 
 ###Prevalence of MAM:
+
+den1 <- with(nut, sum(age >=6 & age < 60, na.rm = TRUE ))
+
+##assigning total number of children aged 6-59 months old to den1
 
 Male_MAM/den1
 
@@ -97,55 +125,47 @@ GB_MAM/den1
 
 ####SAM
 
-with(nut, sum(muac < 11.5  & age >=6 & age <=59 , oedema == 1, na.rm= TRUE))
+with(nut, sum(muac < 11.5  & age >=6 & age <60 , oedema == 1, na.rm= TRUE))
 
 ##number of severe acute malnourished (SAM) cases children 6-59 months old 
 
-Male_SAM <- with(nut, sum(muac < 11.5 & age >=6 & age <=59 & sex == 1, oedema == 1  , na.rm= TRUE ))
+Male_SAM <- with(nut, sum(muac < 11.5 & age >=6 & age <60 & sex == 1, oedema == 1  , na.rm= TRUE ))
 
 ##assigning severe acute malnourished (MAM) female 6-59 months old to Male_SAM
 
-Female_SAM <- with(nut, sum(muac < 11.5 & age >=6 & age <=59 & sex == 2, oedema == 1  , na.rm= TRUE ))
+Female_SAM <- with(nut, sum(muac < 11.5 & age >=6 & age <60 & sex == 2, oedema == 1  , na.rm= TRUE ))
 
 ##assigning severe acute malnourished (MAM) female 6-59 months old to Female_SAM
 
-UM_SAM <- with(nut, sum(muac < 11.5  & age >=6 & age <=59 & county == "Urban Montserrado" ,oedema == 1, na.rm= TRUE))
+with(Male_SAM, data.frame())
+
+UM_SAM <- with(nut, sum(muac < 11.5  & age >=6 & age <60 & county == "Urban Montserrado" ,oedema == 1, na.rm= TRUE))
 
 ##assigning severe acute malnourished (MAM) cases children 6-59 months old  in Urban Montserrado to UM_SAM
 
-GB_SAM <- with(nut, sum(muac < 11.5  & age >=6 & age <=59 & county != "Urban Montserrado" ,oedema == 1, na.rm= TRUE))
+GB_SAM <- with(nut, sum(muac < 11.5  & age >=6 & age <60 & county == "Grand Bassa" ,oedema == 1, na.rm= TRUE))
 
 ##assigning moderate acute malnourished (MAM) cases children 6-59 months old  other than Urban Montserrado to GB_MA
-
-SAM <- data.frame(Male_SAM, Female_SAM, UM_SAM, GB_SAM)
-
-## creating a data frame for SAM
-
-if(interactive()) View(SAM)
-
-##Viewing SAM data
 
 
 ###Prevalence of SAM:
 
-Male_SAM/den1
+Prev1 <- Male_SAM/den1
 
 ##Prevalence of severe acute malnourished (MAM) male 6-59 months old 
 
-Female_SAM/den1
+Prev2 <- Female_SAM/den1
 
 ##Prevalence of severe acute malnourished (MAM) female 6-59 months old 
 
-UM_SAM/den1
+Prev3 <- UM_SAM/den1
 
 ##Prevalence of severe acute malnourished (MAM) cases children 6-59 months old in Urban Montserrado
 
-GB_SAM/den1
+Prev4<- GB_SAM/den1
 
 ##Prevalence of severe acute malnourished (MAM) cases children 6-59 months old other than Urban Montserrado
 
-
-"git status"
 
 
 
