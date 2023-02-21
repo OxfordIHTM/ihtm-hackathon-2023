ff8609810659415e8a92022f4f3dbc7d7ce69e37
Shuma Banik <shuma.banik@vedicascholars.com>
2023-02-21 09:17:54 +0000

    #2nd question

--- coverage_assessment.R
+++ coverage_assessment.R
@@ -52,6 +52,17 @@ nut <- get_data()
 
 # Problem 4: Coverage of severe acute malnutrition treatment ----
 
+#1st question
+total_children <- nrow(nut)
+count(nrow)
+A <- sum(nut$survey_round=="Baseline")
+B <- sum(nut$survey_round=="Endline")
+A+B
+if (nut$survey_round != "Baseline" || "Endline")
+    print("Its the same")
+else 
+    print("False")
+
 
 
 
