63cbec0bc228c611c025b1c339bb051f416f863d
ashishlwh-giri <122526275+ashishlwh-giri@users.noreply.github.com>
2023-03-13 09:35:24 +0000

    Last day:(

--- coverage_assessment.R
+++ coverage_assessment.R
@@ -19,6 +19,7 @@ nut <- get_data()
 
 
 # Problem 1: Describe the data ----
+
 ##Number of children in Baseline
 A <- sum(nut$survey_round == "Baseline")
 ##Number of children in Endline
@@ -28,15 +29,48 @@ sum(A+B)
 ##Checking the total number of data points in survey round
 length(nut$survey_round)
 #So there are no entries other than baseline and endline in survey_round column
-
+table(nut$survey_round, nut$sex, nut$county, useNA = "ifany")
 
 # Problem 2: Screening coverage ----
+##Or you can do it like this for question 1
+#How  many children are there in the overall sample
+sum(is.na(nut$sex))
 
 
+#Total number 
+sum(nut$sex, na.rm = TRUE)
 
+#2. Number of children that are male and female
+table(nut$sex, useNA = "ifany")
 
+#3 Number of children from urban Montserrado and Grand Bassa
+table(nut$county, useNA = "ifany")
 
+# 4and 5 male and female children from Urban Montserrado and males from Grand Bassa
+table(nut$county, nut$sex, useNA = "ifany")
 
+#6 Distribution of ages of children in the overall sample
+table(nut$age)
+summary(nut$age)
+prop.table(table(nut$age, useNA = "ifany"))
+
+#7 Distribution of ages of children by the sex of the child
+table(nut$sex, nut$age)
+scatter.smooth(table(nut$age, nut$sex))
+hist(table(nut$age, nut$sex))
+
+#8 Distribution of ages of children by location
+table(nut$county, nut$age)
+hist(table(nut$age, nut$county))
+scatter.smooth(table(nut$age, nut$county))
+
+#9 Distribution of ages of children by sex and by location
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(data = nut, aes(x = age, fill = sex)) +
+  geom_histogram(alpha = 0.5, position = "identity", bins = 20) +
+  facet_wrap(~county+sex) +
+  labs(title = "Distribution of Ages of Children by Sex and Location", x = "Age", y = "Count")
 
 
 
